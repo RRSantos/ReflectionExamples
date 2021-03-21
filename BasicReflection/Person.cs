222b4c435eed4db69ffd64225fb3316900d66066
Rodrigo Ramos <rodrigo.ramos.santos@gmail.com>
2021-03-21 13:49:59 -0300

    Final version of BasicReflection

---
+++ BasicReflection/Person.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace BasicReflection
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public DateTime BornIn { get; set; }
+
+        public Person()
+        {
+
+        }
+    }
+
+}
---
+++ BasicReflection/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Reflection;
+
+namespace BasicReflection
+{
+    class Program
+    {
+        const string TITLE_SEPARATOR = "=================================================\n\n";
+        const string ACTION_SEPARATOR = "-------------------------------------------------\n";
+        static void Main(string[] args)
+        {
+            Assembly actualAssembly = Assembly.GetExecutingAssembly();
+            Console.WriteLine($"Program's informations\n{TITLE_SEPARATOR}");
+            Console.WriteLine($"Assembly Fullname: {actualAssembly.FullName}");
+
+            Console.WriteLine($"\nList of Defined Types found in this program:\n{TITLE_SEPARATOR}");
+            foreach (TypeInfo t in actualAssembly.DefinedTypes)
+            {
+                Console.WriteLine($"\nType: {t.Name}\n{ACTION_SEPARATOR}");
+                foreach (var m in t.DeclaredMembers)
+                {
+                    Console.WriteLine($"\t{m.MemberType} : {m.Name}");
+                    if (m.MemberType == MemberTypes.Method)
+                    {
+                        MethodInfo mInfo = t.GetMethod(m.Name);
+                        if (mInfo != null)
+                        {
+                            var allParams = mInfo.GetParameters();
+                            if (allParams.Length <= 0)
+                            {
+                                Console.WriteLine($"\t - Params: <none>");
+                            }
+                            else
+                            {
+
+                                Console.WriteLine($"\t - Params:");
+                                foreach (var p in allParams)
+                                {
+                                    Console.WriteLine($"\t\t{p.Name} ({p.GetType()})");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
