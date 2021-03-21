c9811709bef761338c12cca5203f0f41891e3551
Rodrigo Ramos <rodrigo.ramos.santos@gmail.com>
2021-03-21 19:32:39 -0300

    final windows scritps

---
+++ AssemblyInjection/windows/runAssembly2.ps1
@@ -0,0 +1,17 @@
+ .\build.ps1
+$assemblyName = "Assembly2"
+$programFolder = "..\LoadAssembly\bin\Debug\net5.0"
+$loadAssemblyFolder = "$programFolder\Assembly\"
+
+Write-Host "Creating directory '$loadAssemblyFolder'"
+New-Item -ItemType Directory -Force -Path "$loadAssemblyFolder" | Out-Null
+
+Write-Host "Removing *.dll from directory '$loadAssemblyFolder'"
+Remove-Item "$loadAssemblyFolder\*.dll"
+
+Write-Host "Copying $assemblyName to directory '$loadAssemblyFolder'"
+Copy-Item "..\$assemblyName\bin\Debug\net5.0\$assemblyName.dll" -Destination "$loadAssemblyFolder"
+Push-Location "$programFolder"
+.\LoadAssembly.exe
+
+Pop-Location
